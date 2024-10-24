8fc11fcd71647d8a4a5feb97b42b3985270e1af8
Nils Quirmbach <nils.quirmach@outlook.com>
2024-10-24 21:27:59 +0200

    Add tofu backend

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-backend"
+    storage_account_name = "tfbackend1727941381"
+    container_name       = "tfstate"
+    key                  = "k3scluster.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
