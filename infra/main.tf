5caa6c46492b39ed3713d8fa2d6a2e2d99523baf
Nils Quirmbach <nils.quirmach@outlook.com>
2024-10-24 22:44:55 +0200

    Fix artifact path & add vnet

--- infra/main.tf
+++ infra/main.tf
@@ -10,9 +10,18 @@ terraform {
 provider "azurerm" {
   features {}
 }
+provider "random" {
+}
+
+
+resource "random_integer" "rid" {
+  min = 1000
+  max = 9999
+}
 
 locals {
   enviroment = terraform.workspace
+  suffix     = random_integer.rid.result
   tags = {
     env = terraform.workspace
     app = "k3s-cluster"
@@ -23,4 +32,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.resource_group_name}-${terraform.workspace}"
   location = var.location
   tags     = local.tags
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "k3sclustervnet-${local.suffix}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+  tags                = local.tags
 }
