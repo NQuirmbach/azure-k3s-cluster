54ebb4d2c02c43acf0fa83fae2919a5c81c06e95
Nils Quirmbach <nils.quirmach@outlook.com>
2024-10-25 14:33:09 +0200

    fix subnet address

--- infra/main.tf
+++ infra/main.tf
@@ -46,5 +46,5 @@ resource "azurerm_subnet" "cluster-subnet" {
   name                 = "k3sclustersubnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/16"]
+  address_prefixes     = ["10.0.0.0/16"]
 }
